e09660509cf8dd23cf28b97e8cd69c228b1ddab6
Sherif Salama <80130495+sherow1982@users.noreply.github.com>
2025-11-05 12:01:43 +0200

    إضافة السكربتات الجديدة وتحديث البنية

---
+++ scripts/generate-all-product-pages.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
+// نقل المولد الرئيسي إلى مجلد scripts
+// هذا الملف سيتم نسخه من الملف الأصلي
+console.log('يرجى نقل محتوى generate-all-product-pages.js الأصلي إلى هنا');
